ad6d0f875368c4594e16dbb0709325f5c93446e3
manjunadhb <manjunadhb@gmail.com>
2024-11-04 17:35:26 +0530

    added line by manjunadh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
-import StudentTenthMarkSheet from './components/StudentTenthMarkSheet';
+import "./App.css";
+import StudentTenthMarkSheet from "./components/StudentTenthMarkSheet";
 
 function App() {
   return (
     <div className="App">
-    <h1>This line is added by Developer.</h1>
-     <StudentTenthMarkSheet></StudentTenthMarkSheet>
+      <h1>This line is added by Developer.</h1>
+      <h1>This line is added by Manjunadh.</h1>
+      <StudentTenthMarkSheet></StudentTenthMarkSheet>
     </div>
   );
 }

22d735cc8497170c9e7e8992087753b878d782bc
kaaliteja <tejakali303@gmail.com>
2024-11-05 16:12:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>This line is added by Developer.</h1>
       <h1>This line is added by Manjunadh.</h1>
+    <h1>This line is added by kaali Teja.</h1>
       <StudentTenthMarkSheet></StudentTenthMarkSheet>
     </div>
   );
